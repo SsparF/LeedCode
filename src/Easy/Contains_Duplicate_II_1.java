1c3a0d30cde7073e6ba6ad4c5e6713736ad481c5
Ssparf <sashaparfiak007@gmail.com>
2022-10-22 13:02:12 +0300

    add dicription in test 1

--- src/Easy/Contains_Duplicate_II_1.java
+++ src/Easy/Contains_Duplicate_II_1.java
@@ -2,7 +2,9 @@ package Easy;
 
 import java.util.HashMap;
 
-public class Contains_Duplicate_II {
+public class Contains_Duplicate_II_1 {
+/*    Given an integer array nums and an integer k, return true if there are two distinct indices i
+    and j in the array such that nums[i] == nums[j] and abs(i - j) <= k. */
     static boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer, Integer> tempMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
